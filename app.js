b726f78d1672f72ffb3d94f1391dff9929a9665e
luckysharma1502 <110625293+luckysharma1502@users.noreply.github.com>
2022-11-10 02:11:50 +0530

    Add files via upload
    ImBlogger is  a  Blogging website which gives user a freedom to write their views on any topic they want. If you want to write your ideas then you're most welcome.

---
+++ app.js
@@ -0,0 +1,36 @@
+// (function (){
+
+
+//     document.querySelector(".theme-btn").addEventListener("click", () => {
+//         document.body.classList.toggle("light-mode");
+//     })
+// });
+    
+// (function() {
+//     $(document).ready(function() {
+//       $('.switch-input').on('change', function() {
+//         var isChecked = $(this).is(':checked');
+//         var selectedData;
+//         var $switchLabel = $('.switch-label');
+//         console.log('isChecked: ' + isChecked); 
+  
+//         if(isChecked) {
+//           selectedData = $switchLabel.attr('data-on');
+//         } else {
+//           selectedData = $switchLabel.attr('data-off');
+//         }
+  
+//         console.log('Selected data: ' + selectedData);
+  
+//       });
+//     });
+  
+//   })();
+
+
+
+function runme(){
+    alert("Welcome to ImBlogger");
+}
+
+setTimeout(runme,1000);
